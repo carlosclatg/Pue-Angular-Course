838c392c20e5a333114816a05ba34323b2cafac9
carlosclatg <carloscl1986@gmail.com>
2019-10-03 21:48:33 +0200

    con rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from '@angular/router'
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PeopleModule } from './people/people.module';
+
+const routes: Routes = [
+  {path: '' , redirectTo: '/home', pathMatch: 'full'}, //path
+  {path: 'home' , component: HomeComponent },
+  {path: 'about' , component: AboutComponent },   //la props principales son el path y component, hace match de path y muestra comp
+  {path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //Componentes a importar
+    AppComponent,
+    HomeComponent,
+    AboutComponent, 
+    NotFoundComponent
   ],
-  imports: [
+  imports: [ //Modulos a importar, un componente solo puede estar en un modulo.
     BrowserModule,
-    AppRoutingModule
+    PeopleModule, //los modulos con rutas propias tienen que ir antes del modulo raiz
+    RouterModule.forRoot(routes),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], //servicios.
+  bootstrap: [AppComponent] //Componente a levantar en 1 instancia.
 })
 export class AppModule { }
